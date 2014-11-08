2377cf8b79b5617c05d325b21483aebd1e0d91af
brickoflego <googlypantsroberteng@gmail.com>
2014-11-08 11:09:34 -0800

    oops got jquery syntax off a bit

--- public/js/page.js
+++ public/js/page.js
@@ -1,8 +1,8 @@
-$(document).ready() {
-
+$(document).ready(function() {
 	$("#loginButton").click(function(event) {
 		event.preventDefault;
 		console.log("Login button clicked");
 	});
 
-}
+});
+
