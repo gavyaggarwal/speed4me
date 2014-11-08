02111966e828abea034a7e690e375d7cbe176608
Gavy Aggarwal <gavyaggarwal@gmail.com>
2014-11-08 02:48:01 -0800

    DB Attempt 1

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,55 @@
 var express = require('express');
+var DocumentDBClient = require('documentdb').DocumentClient;
 var router = express.Router();
 
+var client = new DocumentDBClient(host, { masterKey: "DCTpmHyuVJMVWMbWMBzaGOGmYlZDV8HmhyTXMXuRSftMyFa3lqlQZfBEI6eGYbNDqeMBRrYXLKX8tlhrdQ4JcQ==" });
+
 router.get('/login', function(req, res) {
-  var username = req.headers;
+  /* Request header:
+    { username: <username> }
+  Response:
+    {
+      'success': true,
+      'username': <username>
+    }
+  */
+
+  var username = req.headers["username"];
   var result = JSON.stringify({
     'success':true,
     'username':username
   });
   res.writeHead(200, {"Content-Type": "application/json"});
-  res.end(" EG " + result );
+  res.end(result);
 });
+
+router.get('/getDrops', function(req, res) {
+  /* Request header:
+    { username: <username> }
+  Response:
+    {
+      'success': true,
+      'username': <username>
+    }
+  */
+
+  var username = req.headers["username"];
+  var result = JSON.stringify({
+    'success':true,
+    'username':username
+  });
+  readOrCreateDatabase(function (database) {
+    result['db'] = database;
+    readOrCreateCollection(database, function (collection) {
+      result['col'] = collection
+      listItems(collection, function (items) {
+        //res.render('index', { title: 'My ToDo List', tasks: items });
+      });
+    });
+  });
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(result);
+});
+
 
 module.exports = router;
