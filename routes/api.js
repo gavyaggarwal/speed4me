36281c206e719358ae85c3dca51a354426b59bcc
Gavy Aggarwal <gavyaggarwal@gmail.com>
2014-11-08 02:32:32 -0800

    Login working

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/login', function(req, res) {
   var username = req.headers;
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end({
+  var result = JSON.stringify({
     'success':true,
     'username':username
   });
-  //res.render('index', { title: 'Express' });
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(" EG " + result );
 });
 
 module.exports = router;
